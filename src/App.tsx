92c989af286a38ad27b32c53bab52ebddd7c2be3
Artur Wiśniewski <artixbtw@gmail.com>
2023-12-20 17:56:25 +0100

    add basic React Router logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import { Home } from "./pages/Home";
+import { AppLayout } from "./ui/AppLayout";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 export const App = () => {
-  return <Home />;
+  return <RouterProvider router={router} />;
 };
