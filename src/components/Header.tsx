a0771f4b4eaf233e096f5a379764ed7585900430
Artur Wiśniewski <artixbtw@gmail.com>
2023-12-31 12:52:57 +0100

    add logic to button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+type Theme = "light" | "dark";
+
 export const Header = () => {
+  const [theme, setTheme] = useState<Theme>(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light",
+  );
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
     <header className="border-b">
       <div className="mx-auto flex max-w-screen-2xl justify-between p-5 max-sm:p-3">
@@ -9,7 +30,7 @@ export const Header = () => {
             Where in the world?
           </h1>
         </Link>
-        <button>Dark Mode</button>
+        <button onClick={handleThemeSwitch}>Dark Mode</button>
       </div>
     </header>
   );
